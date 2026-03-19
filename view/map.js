5fb428b796ea118af96b7497b5d31ccda0aea37f
Kirill <kirillun1027@gmail.com>
2026-03-19 11:06:19 +0500

    add map

---
+++ view/map.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([
+    43.676, 77.116
+], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
